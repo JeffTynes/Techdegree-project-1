f2420aae805589674bf0018beb74eceb2c691ecf
Jeffrey Tynes <jeffrey_tynes@outlook.com>
2018-05-20 04:29:17 -0300

    Random colors
    Added getRandomColor function to generate and return a random hexadecimal color
    
    Added changeBackGround  function which changes the background color of it's arguments
    
    removed "content: ',  ';" from .citation:before in styles.css so there isn't a comma at the start of a new line
    
    added lastQuoted and a do while loop to getRandomQuote so the last displayed quote can't be the first quote chosen when 'quotes' is refreshed
    
    added setInterval to display a new quote every 30s
    
    added .5 transition to the body so the color changes at the same speed as the button's color

--- js/script.js
+++ js/script.js
@@ -1,30 +1,76 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+const button = document.getElementById('loadQuote');
 const quoteBox = document.getElementById('quote-box');
 const body = document.getElementsByTagName('BODY')[0];
 
-const quotes = [
+button.addEventListener("click", () => {
+	printQuote();
+	changeBackGround(getRandomColor(), body, button);
+	clearTimeout(newQuote);
+	newQuote = setInterval(() => {
+		printQuote();
+		changeBackGround(getRandomColor(), body, button);
+	}, 30000);
+}, false);
+
+let quotes = [
 	{quote: 'The secret of getting ahead is getting started', source: 'Mark Twain'},
-	{quote: 'The truth is rarely pure and never simple', source: 'Oscar Wilde'},
-	{quote: 'No act of kindnes, no matter how small, is ever wasted', source: 'Aesop'},
-	{quote: 'If opportunity doesn\'t knock, build a door', source: 'Milton Berle'},
-	{quote: 'The measure if who we are is what we do with what we have', source: 'Vince Lombardi'},
-	{quote: 'Look deep into nature, and then you will understand everything better', source: 'Albert Einstein'}
+	{quote: 'The truth is rarely pure and never simple', source: 'Oscar Wilde', citation: 'The Importance of Being Earnest', year: 1895, tags: '(truth)'},
+	{quote: 'No act of kindnes, no matter how small, is ever wasted', source: 'Aesop', citation: 'The Lion and the Mouse', tags: '(kindness)'},
+	{quote: 'If opportunity doesn\'t knock, build a door', source: 'Milton Berle', tags: '(motivation)'},
+	{quote: 'The measure of who we are is what we do with what we have', source: 'Vince Lombardi', tags: '(inspration)'},
+	{quote: 'Look deep into nature, and then you will understand everything better', source: 'Albert Einstein', tags: '(inspration, nature)'}
 ];
 
+let quoted = [];
+let lastQuoted = [];
+
 function getRandomQuote() {
-	return quotes[Math.floor(Math.random() * quotes.length)];
+	if(!quotes.length) {
+		quotes = quoted;
+    quoted = [];
+	}
+
+	let quote;
+
+	do {
+		quote = quotes[Math.floor(Math.random() * quotes.length)];
+	}
+	while(quote == lastQuoted[0])
+
+	quoted.push(quote);
+	lastQuoted[0] = quote;
+	quotes = quotes.filter(el => el !== quote);
+	return quote;
 }
 
 function printQuote() {
 	const quote = getRandomQuote();
+	console.log(quote.quote);
 	let quoteHtml = `<p class='quote'> ${quote.quote} </p>`;
 	quoteHtml += `<p class='source'> ${quote.source}`;
-	quoteHtml += quote.citation ? `<span class='citation'> ${quote.citation} </span>` : '';
+	quoteHtml += quote.citation ? `<br/><span class='citation'> ${quote.citation} </span>` : '';
 	quoteHtml += quote.year ? `<span class='year'> ${quote.year} </span>` : '';
+	quoteHtml += quote.tags ? `<span class='tags'> ${quote.tags} </span>` : '';
 	quoteHtml += '</p>';
 	quoteBox.innerHTML = quoteHtml;
 }
-printQuote();
 
+function getRandomColor() {
+  let color = '#';
+  while(color.length < 7) {
+  	color += Math.floor(Math.random() * 16).toString(16);
+  }
+  return color;
+}
+
+function changeBackGround(color, ...args) {
+  args.forEach(el => el.style.backgroundColor = color);
+}
+
+let newQuote = setInterval(() => {
+ printQuote();
+ changeBackGround(getRandomColor(), body, button);
+}, 30000);
+
+printQuote();
+changeBackGround(getRandomColor(), body, button);
